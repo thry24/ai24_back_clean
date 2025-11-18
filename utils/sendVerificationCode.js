6b68acfd2184f09f3683e2d54b8bf18f42f8806d
thry24 <contacto@thry24.com.mx>
2025-11-18 13:32:53 -0600

    prueba con puerto distinto

--- utils/sendVerificationCode.js
+++ utils/sendVerificationCode.js
@@ -4,7 +4,7 @@ const EmailVerification = require("../models/EmailVerification");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,

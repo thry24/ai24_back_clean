f3a09085179b5d7d1cec0efdaed52ae6d868a2ba
thry24 <contacto@thry24.com.mx>
2025-12-26 11:38:43 -0600

    port

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.hostinger.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: 'verificaciones@ai24mx.com',  // tu cuenta en Hostinger
     pass: 'ai24EkJc-2025-src'           // tu contraseña SMTP
